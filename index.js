937c60ea1f72847b4a6eaa35f24d48bd2291f0ac
Justin W Soto <soto@pdx.edu>
2021-06-23 14:40:52 -0700

    finished JS basics

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
-
 //nametag
-    const nameTag = document.getElementById('name-tag');
-    console.log(nameTag);
+const nameTag = document.getElementById('name-tag');
+console.log(nameTag);
 
 //top-section
-    const topSection = document.getElementById('top-section');
-    topSection.style.backgroundColor = 'salmon';
-    topSection.style.color = 'white';
+const topSection = document.getElementById('top-section');
+topSection.style.backgroundColor = 'red';
+topSection.style.color = 'white';
 
 //bottom-section
-    const bottomSection = document.getElementById('bottom-section');
-    bottomSection.style.backgroundColor = 'salmon';
+const bottomSection = document.getElementById('bottom-section');
+bottomSection.style.backgroundColor = 'red';
 
 //name-section
-    const nameSection = document.getElementById('name-section');
-    nameSection.innerText = 'Kilgore Trout';
-    nameSection.style.color = 'green';
+const nameSection = document.getElementById('name-section');
+nameSection.innerText = 'Kilgore Trout';
+nameSection.style.color = 'green';
 
 //input-field
-    const nameInput = document.getElementById('name-input');
-    // nameInput.addEventListener('input', (e)=>{
-    // console.log(e.target.value)
-    // nameSection.innerText = e.target.value;
-    
- //});
+const nameInput = document.getElementById('name-input');
 
-//button 
-    const submitButton = document.getElementById('submit-button');
-    submitButton.addEventListener('click', (e)=>{
-    nameSection.innerText = nameInput.value;
+//name-button 
+const submitButton = document.getElementById('submit-button');
+submitButton.addEventListener('click', (e)=>{
+nameSection.innerText = nameInput.value;
 });
 
+//pronoun-button
+const pronounButton = document.getElementById('pronoun-button');
+pronounButton.addEventListener('click', (e)=>{
+pronounSection.innerText = pronounInput.value;    
+})
+//pronoun-input
+const pronounInput = document.getElementById('pronoun-input');
+const pronounSection = document.getElementById('pronoun-section');
+pronounSection.innerText = 'he/him'
+pronounSection.style.color = 'green';
 
+//color-buttons
+const blueButton = document.getElementById('color-blue');
+const pinkButton = document.getElementById('color-pink');
+const greenButton = document.getElementById('color-green');
 
+blueButton.addEventListener('click', (e)=>{
+    topSection.style.backgroundColor = 'blue';
+    bottomSection.style.backgroundColor = 'blue';
+});
 
+pinkButton.addEventListener('click', (e)=>{
+    topSection.style.backgroundColor = 'salmon';
+    bottomSection.style.backgroundColor = 'salmon'
+});
+
+greenButton.addEventListener('click', (e)=>{
+    topSection.style.backgroundColor = 'green';
+    bottomSection.style.backgroundColor = 'green';
+})
+    
